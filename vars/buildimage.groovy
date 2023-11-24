03123640ea953564ba6444e51d6bc43c99328f4b
busolagbadero <oguneye.lami@gmail.com>
2023-11-24 04:56:51 +0000

    first commit

---
+++ vars/buildimage.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+def call() {
+
+    echo 'Building Image...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        sh 'docker build -t gbaderobusola/busola:tolani-1.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push gbaderobusola/busola:tolani-1.0'
+        }
+}
