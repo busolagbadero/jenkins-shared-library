485b0748d7cca325a31e88de332b6cd69cd7fbed
busolagbadero <94229949+busolagbadero@users.noreply.github.com>
2023-11-25 15:55:41 +0000

    Update buildimage.groovy

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
-def call() {
+def call(String imageName) {
 
     echo 'Building Image...'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'docker build -t gbaderobusola/busola:tolani-1.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push gbaderobusola/busola:tolani-1.0'
+        sh "docker $imageName"
         }
 }
